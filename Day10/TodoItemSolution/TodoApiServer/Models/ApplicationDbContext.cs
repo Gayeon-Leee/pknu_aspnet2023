7a33e1a968c7a665ce3fcd4445d8adef9545abdd
Gayeon_Lee913 <gy9411@naver.com>
2023-05-31 12:26:39 +0900

    5/31 RestAPI

---
+++ Day10/TodoItemSolution/TodoApiServer/Models/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace TodoApiServer.Models
+{
+    public class ApplicationDbContext : DbContext
+    {
+        //생성자는 생성자 마법사로 만들면됨 (option)
+        public ApplicationDbContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        // DB 연결에 필수
+        public DbSet<TodoItem> TodoItems { get; set; } 
+    }
+}
